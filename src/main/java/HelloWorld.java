501e5f54849cfeb6d699efcb8453113170e6b3a8
Ubuntu <ubuntu@ip-172-31-92-234.ec2.internal>
2026-08-05 14:59:38 +0000

    Added Java source for SonarQube

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins SonarQube");
+    }
+
+    public String message() {
+        return "Hello Jenkins SonarQube";
+    }
+}
